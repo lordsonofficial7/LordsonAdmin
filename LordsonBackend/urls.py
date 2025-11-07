93526e8cb6b1736783e5e38d2cd5a714ca17506b
freakfit22 <freakfit22@gmail.com>
2025-11-07 19:12:37 +0530

    Vercel Commit12

--- LordsonBackend/urls.py
+++ LordsonBackend/urls.py
@@ -33,3 +33,5 @@ urlpatterns = [
 # 👇 Serve uploaded media files (like banner images) during development
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
