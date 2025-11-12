3d076a94e498906a8a460a916178c8ccbadd20a7
freakfit22 <freakfit22@gmail.com>
2025-11-12 18:05:05 +0530

    Vercel issu36

--- LordsonBackend/wsgi.py
+++ LordsonBackend/wsgi.py
@@ -1,17 +1,7 @@
-import sys
-import traceback
+import os
+from django.core.wsgi import get_wsgi_application
 
-try:
-    from django.core.wsgi import get_wsgi_application
-    import os
-    import sys
 
-    sys.path.append(os.path.dirname(os.path.dirname(__file__)))
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'LordsonBackend.settings')
-
-    application = get_wsgi_application()
-    app = application
-except Exception as e:
-    print("🔥 Django WSGI startup error:", e, flush=True)
-    traceback.print_exc()
-    sys.exit(1)
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'LordsonBackend.settings')
+application = get_wsgi_application()
+app = application
